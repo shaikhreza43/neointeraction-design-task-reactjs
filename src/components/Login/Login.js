87f5dc676f7872c75bad9ae3f20f278f54f3f6eb
shaikhreza43 <reza.rk786@gmail.com>
2020-07-28 22:01:47 +0530

    Image alignment issue fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,13 +6,10 @@ import { Link } from 'react-router-dom';
 
 const LoginFormDiv = styled.div`
 border:1px solid #000000;
-padding:20px 15px 350px 15px;
-border-radius:10px;
-position:fixed;
-margin-left:1000px;
-overflow-x:hidden;
+padding:20px 15px 289px 15px;
 box-shadow:1px 2px #000000;
 background-color:lightgray;
+overflow:hidden;
  `
 
 const RandomImageDiv = styled.div`
@@ -20,7 +17,10 @@ background-image:url(https://source.unsplash.com/random);
 background-position:center;
 background-size:cover;
 background-repeat: no-repeat;
- `
+height:100%;
+overflow:hidden;
+margin-right:-30px;
+`
 
 class Login extends Component {
 
@@ -35,50 +35,52 @@ class Login extends Component {
     render() {
         return (
             <Fragment>
+                <Container fluid>
+                    <Row>
+                        <Col lg="7" md="8">
+                            <RandomImageDiv></RandomImageDiv>
+                        </Col>
+                        <Col lg="5" md="5">
+                            <LoginFormDiv>
+                                <Form onSubmit={this.handleSubmit}>
 
-                <RandomImageDiv>
-                    
-                </RandomImageDiv>
-                {/* ------------------------------------------------------------------------------ */}
-                <LoginFormDiv>
-
-                    <Form onSubmit={this.handleSubmit}>
-                        <Row>
-                            <Col lg="12">
-                                <FormGroup>
-                                    <Label for="email">Email</Label>
-                                    <Input type="text" name="email" placeholder="Enter Email..."></Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <FormGroup>
-                                    <Label for="password">Password</Label>
-                                    <Input type="password" name="password" placeholder="Enter Password..."></Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <Button type="submit" color="secondary">Login</Button>
-                                <Label className="ml-2">
-                                    Don't have an account?
-                                    {"  "}
-                                    <Link to="/register">Register</Link>
-                                </Label>
-                            </Col>
-                        </Row>
-                    </Form>
-
-                    <ul className="mt-5">
-                        <li>Valid Email Should be entered in the email input</li>
-                        <li>Password should be minimum 8 characters long and it should be the combination of both numbers and alphabets</li>
-                    </ul>
+                                    <Col lg="12">
+                                        <FormGroup>
+                                            <Label for="email">Email</Label>
+                                            <Input type="text" name="email" placeholder="Enter Email..."></Input>
+                                        </FormGroup>
+                                    </Col>
 
 
-                </LoginFormDiv>
+                                    <Col lg="12">
+                                        <FormGroup>
+                                            <Label for="password">Password</Label>
+                                            <Input type="password" name="password" placeholder="Enter Password..."></Input>
+                                        </FormGroup>
+                                    </Col>
 
+
+                                    <Col lg="12">
+                                        <Button type="submit" color="secondary">Login</Button>
+                                        <Label className="ml-2">
+                                            Don't have an account?
+                                                 {"  "}
+                                            <Link to="/register">Register</Link>
+                                        </Label>
+                                    </Col>
+
+                                </Form>
+
+                                <ul className="mt-5">
+                                    <li>Valid Email Should be entered in the email input</li>
+                                    <li>Password should be minimum 8 characters long and it should be the combination of both numbers and alphabets</li>
+                                </ul>
+
+                            </LoginFormDiv>
+
+                        </Col>
+                    </Row>
+                </Container>
             </Fragment>
         )
     }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React,{Component,Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import './Register.css';
 import { FormGroup, Input, Form, Label, Button, Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
@@ -6,13 +6,10 @@ import { Link } from 'react-router-dom';
 
 const RegisterFormDiv = styled.div`
 border:1px solid #000000;
-padding:20px 15px 350px 15px;
-border-radius:10px;
-position:fixed;
-margin-left:1000px;
-overflow-x:hidden;
+padding:20px 15px 203px 15px;
 box-shadow:1px 2px #000000;
 background-color:lightgray;
+overflow:hidden;
  `
 
 const RandomImageDiv = styled.div`
@@ -20,7 +17,10 @@ background-image:url(https://source.unsplash.com/random);
 background-position:center;
 background-size:cover;
 background-repeat: no-repeat;
- `
+height:100%;
+overflow:hidden;
+margin-right:-30px;
+`
 
 class Register extends Component {
 
@@ -35,58 +35,59 @@ class Register extends Component {
     render() {
         return (
             <Fragment>
-
-                <RandomImageDiv>
-                    
-                </RandomImageDiv>
-                {/* ------------------------------------------------------------------------------ */}
-                <RegisterFormDiv>
-
-                    <Form onSubmit={this.handleSubmit}>
+                <Container fluid>
                     <Row>
-                            <Col lg="12">
-                                <FormGroup>
-                                    <Label for="username">Username</Label>
-                                    <Input type="text" name="username" placeholder="Enter Username..."></Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <FormGroup>
-                                    <Label for="email">Email</Label>
-                                    <Input type="text" name="email" placeholder="Enter Email..."></Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <FormGroup>
-                                    <Label for="password">Password</Label>
-                                    <Input type="password" name="password" placeholder="Enter Password..."></Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <Button type="submit" color="secondary">Register</Button>
-                                <Label className="ml-2">
-                                    Already have an account!
-                                    {"  "} 
-                                    <Link to="/login">Login</Link>
-                                </Label>
-                            </Col>
-                        </Row>
-                    </Form>
+                        <Col lg="7" md="8">
+                            <RandomImageDiv></RandomImageDiv>
+                        </Col>
+                        <Col lg="5" md="5">
+                            <RegisterFormDiv>
+                                <Form onSubmit={this.handleSubmit}>
 
-                    <ul className="mt-5">
-                        <li>Valid Email Should be entered in the email input</li>
-                        <li>Password should be minimum 8 characters long and it should be the combination of both numbers and alphabets</li>
-                    </ul>
+                                <Col lg="12">
+                                        <FormGroup>
+                                            <Label for="username">Username</Label>
+                                            <Input type="text" name="username" placeholder="Enter Username..."></Input>
+                                        </FormGroup>
+                                    </Col>
+
+                                    <Col lg="12">
+                                        <FormGroup>
+                                            <Label for="email">Email</Label>
+                                            <Input type="text" name="email" placeholder="Enter Email..."></Input>
+                                        </FormGroup>
+                                    </Col>
 
 
-                </RegisterFormDiv>
+                                    <Col lg="12">
+                                        <FormGroup>
+                                            <Label for="password">Password</Label>
+                                            <Input type="password" name="password" placeholder="Enter Password..."></Input>
+                                        </FormGroup>
+                                    </Col>
 
+
+                                    <Col lg="12">
+                                        <Button type="submit" color="secondary">Register</Button>
+                                        <Label className="ml-2">
+                                            Already have an account?
+                                                 {"  "}
+                                            <Link to="/login">Login</Link>
+                                        </Label>
+                                    </Col>
+
+                                </Form>
+
+                                <ul className="mt-5">
+                                    <li>Valid Email Should be entered in the email input</li>
+                                    <li>Password should be minimum 8 characters long and it should be the combination of both numbers and alphabets</li>
+                                </ul>
+
+                            </RegisterFormDiv>
+
+                        </Col>
+                    </Row>
+                </Container>
             </Fragment>
         )
     }
